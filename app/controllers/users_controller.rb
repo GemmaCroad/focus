a8c20328b2b241a1de39d8edc76578fd092fb3fb
Gemma Stiles <gemma.stiles@gmail.com>
2015-09-10 12:01:24 +1000

    styling part one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,11 +32,11 @@ class UsersController < ApplicationController
       end
   end
 
-  # def destroy
-  #   user = User.find params[:id]
-  #   user.destroy
-  #   redirect_to users_path
-  # end
+  def destroy
+    user = User.find params[:id]
+    user.destroy
+    redirect_to users_path
+  end
 
   def photos
     @user = User.find params[:user_id]
